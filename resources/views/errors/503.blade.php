0345c46303358c5226ac7185656771eb5cf7a19a
ejuarezc <eric.jrz@gmail.com>
2019-08-15 09:40:56 -0700

    Crea la plantilla personalizado para el error 503, servicio fuera de linea

---
+++ resources/views/errors/503.blade.php
@@ -0,0 +1,22 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+	<meta charset="UTF-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1.0">
+	<meta http-equiv="X-UA-Compatible" content="ie=edge">
+	<link rel="stylesheet" href="/css/bootstrap.min.css" />
+	<title>Thanks! - Savingwatts Team</title>
+</head>
+<body>
+	<div class="container mt-5">
+		<div class="display-2 text-center">Savingwatts</div>
+		<div class="text-right">
+			<p class="lead mt-5" style="font-size: 1.5rem;">
+				Agradecemos ver presenciado nuestra presentación del proyecto. <br>
+				Nos vemos en Diciembre, 2019.
+			</p>
+			<div class="blockquote-footer" style="font-size: 1.2rem;">Savingwatts Team</div>
+		</div>
+	</div>
+</body>
+</html>
